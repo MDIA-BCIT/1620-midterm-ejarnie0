9adbbcb0c3f10888a7ec9a5af51c1507e4783fa7
Emz <ejennings13@my.bcit.ca>
2024-11-01 15:10:09 -0700

    Adding my answer for the midterm question!
    Created a function for a password check, user's correct password is "jenis1" :)

--- problem.js
+++ problem.js
@@ -13,3 +13,35 @@ CHALLENGE
 - If the password is less than 5 characters, log out "Your password is too short!".
 */
 
+
+/*
+correctPassword = jenis1
+userInput = whatever they write
+requiredLength = 6
+forgotPassword = Here is a hint + "This password you set should not be used because it glitches the system"
+resetPassword = Let's reset your account + "This password you set should not be used because it glitches the system"
+
+passwordCheck("jenis1", "", 6, "forgot", "reset") ! !wrong don't use this! !
+*/
+
+
+function passwordCheck(correctPassword, userInput, requiredLength, forgotPassword, resetPassword){
+    if(userInput.length === requiredLength && correctPassword) {
+        console.log("Access Granted!");
+    } else if(userInput.length != requiredLength && correctPassword) {
+        console.log("Access Denied!");
+    } else if(userInput === forgotPassword) {
+        console.log("Here's a hint." + " This password you set should not be used because it glitches the system!");
+    } else if(userInput === resetPassword) {
+        console.log("Let's reset your account." + " This password you set should not be used because it glitches the system!");
+    } else if(userInput != requiredLength) {
+        console.log("Your password is too short!");
+    }
+}
+
+
+passwordCheck("jenis1", "jenis1", 6, "forgot", "reset") /* "Access Granted!" */
+passwordCheck("jenis1", "shmenis2", 6, "forgot", "reset") /* "Access Denied!" */
+passwordCheck(undefined, "forgot", undefined, "forgot", undefined) /* "Here's a hint" "This password you set should not be used because it glitches the system" */ 
+passwordCheck(undefined, "reset", undefined, undefined, "reset") /* "Let's reset your account" */
+passwordCheck(undefined, "jenis", 6, undefined, undefined) /* "Your password is too short!" */
